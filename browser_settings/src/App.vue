fc47d56be54d8d4a33df42da7b1f940b8765a060
Mikhail A <mg@abramishvili.net>
2022-05-23 14:32:38 +0500

    .

--- browser_settings/src/App.vue
+++ browser_settings/src/App.vue
@@ -1,21 +1,42 @@
 <template>
-    <div class="container">
-        <div class="mb-3">
-            <input v-model="configuration.homepage" type="text" class="form-control">
-        </div>
-        <ul class="list-group">
-            <li v-for="item in configuration.whitelist" class="list-group-item">
-                {{ item }}
-                <button @click="delWhitelistItem(item)" class="btn btn-sm btn-outline-danger">&times;</button>
-            </li>
-        </ul>
-
-        <div>
-            <input v-model="whitelistItem" type="text" class="form-control">
-            <button @click="addWhitelistItem()" class="btn btn-primary">OK</button>
+    <div class="wrapper">
+        <div class="bg-primary py-3 mb-4">
+            <div class="container">
+                <h1 class="m-0 text-white">Настройки браузера</h1>
+            </div>
         </div>
 
-        <button @click="save()" class="btn btn-lg btn-primary">Сохранить настройки</button>
+        <div class="container">
+            <div v-if="views.success" class="alert alert-success mb-4">
+                Настройки сохранены
+            </div>
+
+            <div class="bg-light px-4 py-4 border mb-4">
+                <h5 class="mt-0 mb-3">Домашняя страница</h5>
+                <input v-model="configuration.homepage" type="text" class="form-control">
+            </div>
+
+            <div class="bg-light px-4 py-4 border mb-4">
+                <h5 class="mt-0 mb-3">Разрешенные адреса</h5>
+                <ul class="list-group mb-1">
+                    <li v-for="item in configuration.whitelist" class="list-group-item d-flex align-items-center justify-content-between">
+                        {{ item }}
+                        <button @click="delWhitelistItem(item)" class="btn btn-sm btn-outline-danger">&times;</button>
+                    </li>
+                </ul>
+                
+                <div class="row">
+                    <div class="col-10">
+                        <input v-model="whitelistItem" type="text" placeholder="добавление нового адреса..." class="form-control">
+                    </div>
+                    <div class="col-2">
+                        <button @click="addWhitelistItem()" class="btn btn-outline-primary w-100">OK</button>
+                    </div>
+                </div>
+            </div>
+
+            <button @click="save()" class="btn btn-primary mb-4">Сохранить настройки</button>
+        </div>
     </div>
 </template>
 
@@ -26,6 +47,10 @@
                 configuration: '',
                 
                 whitelistItem: '',
+
+                views: {
+                    success: false,
+                }
             }
         },
         created() {
@@ -59,8 +84,32 @@
                 })
                 .then(response => {
                     this.loadConfiguration()
+
+                    window.scrollTo(0, 0)
+
+                    this.views.success = true
+
+                    setTimeout(() => {
+                        this.views.success = false
+                    }, 2000)
                 })
             },
         }
     }
 </script>
+
+<style>
+    .bg-light {
+        border-radius: 6px;
+    }
+    h1 {
+        font-size: 22px;
+        text-transform: uppercase;
+        line-height: 1;
+    }
+    h5 {
+        text-transform: uppercase;
+        color: #454545;
+        font-size: 16px;
+    }
+</style>
